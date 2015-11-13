ec2a8fb8e0518554661c14fcbf35e2f63fc4c112
maoten <maotenzzz10@gmail.com>
2015-11-12 21:53:16 -0500

    validaciones

---
+++ proyectoPhpSubasta/js/main.js
@@ -0,0 +1,84 @@
+$("#form").submit(function(e){
+	e.preventDefault();
+});
+
+
+$("#boton").click(function(){
+
+	var validator = $("#form").validate({
+		rules:{
+			email:"required",    
+            
+			name:
+				"required" 
+                ,
+			nick:{
+				required:true,
+				minlength:9
+			},
+			password:{
+				required:true,
+				minlength:5
+			},
+          
+			confirm:{
+				required:true,
+				minlength:5
+			},
+			direccion:{
+				required:true,
+
+			},
+			cedula:{
+				required:true,
+				minlength:11
+			},
+            tel:{
+                required:true,
+                minlength:10,
+                digits:true
+            }
+            
+
+		},messages:{
+			
+            name:{
+              required:"Ingrese un nombre"  
+            },
+			nick:{
+				minlength:"Por favor ingrese al menos un nick de 9 cracteres",
+				required:"Por favor ingrese un nick"
+			},
+			email:{
+				required:"Por favor ingrese una dirección de correo electronico",
+				email:"Ingrese una dirección valida"
+			},
+			password:{
+				required:"Por favor ingrese una contraseña",
+				minlength: "La contraseñadebe ser de al menos 5 digitos"
+			},
+			confirm:{
+				required:"Por favor ingrese una contraseña",
+				minlength: "La contraseñadebe ser de al menos 5 digitos"
+			},
+			direccion:{
+				required:"Por favor ingrese una direccion"
+			},
+			cedula:{
+				required:"Ingrese un número de cedula",
+				cedula:"Ingrese 11 digitos"
+			},
+            tel:{
+				required:"Ingrese un número telefónico",
+				minlength:"Ingrese 10 digitos",
+                digits:"Ingrese solo números"
+			}
+
+
+
+			}
+	});
+	
+});
+
+
